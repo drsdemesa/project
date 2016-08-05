48212d90104849df52bf18c890d3e31f525555de
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2016-08-05 13:59:02 +0800

    Added a layout template for view files

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,44 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>About Page</title>
+@extends('layout')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('header-section')
+    About Us
+@stop
 
-        <style>
-            html, body {
-                height: 100%;
-            }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
+@section('content-variable')
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
         <div class="container">
             <div class="content">
-                <div class="title">About Page</div>
+                <div class="title">About Page Here</div>
                 @unless (empty($people)) 
                     There are some people.
                 @else 
@@ -54,5 +25,4 @@
                 @endforeach
             </div>
         </div>
-    </body>
-</html>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layout')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('header-section')
+    Laravel - Home
+@stop
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+@section('content-variable')
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 - Boot up page here</div>
             </div>
         </div>
-    </body>
-</html>
+@stop
